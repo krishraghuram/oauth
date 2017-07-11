a0fd48e008472f5276e9573261b7995d903d032a
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2017-07-11 14:05:24 +0530

    Cleaned up login. Added login and signup success pages. Wrote Shorthand Docs for all views in testoauth.

--- testoauth/urls.py
+++ testoauth/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^login/' , views.LoginView.as_view() , name='login'),
     url(r'^signup/', views.SignupView.as_view(), name='signup'),
     url(r'^logout/', views.LogoutView.as_view(), name='logout'),
+    url(r'^login_success/' , views.LoginSuccessView.as_view() , name='login_success'),
+    url(r'^signup_success/' , views.SignupSuccessView.as_view() , name='signup_success'),
 ]
