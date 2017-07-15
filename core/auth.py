c62a033b1862702dd6eb08282dda4a9aaa3b7189
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2017-07-15 20:10:30 +0530

    Cleanup in WebmailAuthenticationBackend

--- core/auth.py
+++ core/auth.py
@@ -5,18 +5,7 @@ from django.contrib.auth import get_user_model
 import django.core.exceptions
 
 class WebmailAuthenticationBackend(object):
-    '''
-    Authenticate against IIT Guwahati Webmail Servers.
-    Use the webmail id, password(not saved), IP of login server and valid port.
-    Default port is set to ``poplib.POP3_SSL_PORT``.
-    '''
-
     def get_user(self, user_id):
-        '''
-        Return user object.
-        :param user_id: primary key of user object
-        :return: user object
-        '''
         user_model = get_user_model()
         try:
             return user_model.objects.get(id=user_id)
@@ -24,46 +13,41 @@ class WebmailAuthenticationBackend(object):
             return None
 
     def authenticate(self, request, **credentials):
-        '''
-        Returns user for credentials provided if credentials are valid.
-        Returns ``None`` otherwise.
-        :param credentials: keyword arguments
-        :return: user object
-        '''
         socket.setdefaulttimeout(5)
-        webmail = credentials.get('webmail').split('@')[0]
+        # Todo Cleanup
+        # No need to sanitize anymore, since forms sanitize all user input data.
+        webmail = credentials.get('webmail')
         password = credentials.get('password')
         mail_server = credentials.get('mail_server')
-        port = credentials.get('port', poplib.POP3_SSL_PORT)
 
         user_model = get_user_model()
         try:
-            kw = {user_model.USERNAME_FIELD+'__contains' : webmail} #Need to use contains because of split in line 33
+            # Todo Cleanup
+            # No need to use contains anymore, since forms sanitize all user input data.
+            kw = {user_model.USERNAME_FIELD : webmail}
             user = user_model.objects.get(**kw) 
         except user_model.DoesNotExist:
             #No raise. Its perfectly fine, as user object wont exist during first time login. The view handling authentication will handle this case.
             return None 
 
         try:
-            conn = poplib.POP3_SSL(host=mail_server, port=port)
+            conn = poplib.POP3_SSL(host=mail_server, port=poplib.POP3_SSL_PORT)
             conn.user(webmail)
             if 'OK' in conn.pass_(password):
                 conn.quit()
                 return user
         except poplib.error_proto:
+            #For development use only. Use logging later.
             print  "\n"
-            print "Poplib Error Proto"
-            print "There are multiple reasons for this error. Check Poplib documentation for details."
+            print "Poplib Error Proto" 
             print "\n"
             raise #Re-Raise the exception
         except socket.error as e:
             s = e.message
             if e.message=="timed out":
                 s = "Socket Timed Out."
+            #For development use only. Use logging later.
             print  "\n"
             print s
             print "\n"
             raise #Re-Raise exception
-        except (ValueError, TypeError) as e:
-            raise e
-
